575c1e947db00eba6989eabde606139c5cc63fe3
ItsUpToYou <v_kapitanov@yahoo.com>
2016-03-11 02:04:20 +0200

    C# week07

--- Week07/01-Linq-Basics/AnonymousMethod/AverageAggregator.cs
+++ Week07/01-Linq-Basics/AnonymousMethod/AverageAggregator.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnonymousMethod
 {
-    class AverageAggregator
+    public class AverageAggregator : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double average;
+
+        private readonly List<int> nums = new List<int>();
+
+        public double Average
+        {
+            get
+            {
+                return this.average;
+            }
+            set
+            {
+                if (this.average != value && this.PropertyChanged != null)
+                {
+                    this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Average"));
+                }
+                this.average = value;
+            }
+        }
+
+        public void AddNumber(int num)
+        {
+            this.nums.Add(num);
+            this.Average = this.nums.Average();
+        }
+
     }
 }
