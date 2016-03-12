07c9ddd4d3527f9b292a36624e9852e18e8a8b6a
ItsUpToYou <v_kapitanov@yahoo.com>
2016-03-12 17:24:55 +0200

    C# week05

---
+++ Week05/01-SortingAndSearching/SelectionSort/LastDigitComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    public class LastDigitComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            int first = x;
+            int second = y;
+
+            int firstLenght = x.ToString().Length;
+            int secondLenght = y.ToString().Length;
+
+            if (firstLenght > 1)
+            {
+                first = int.Parse(x.ToString()[firstLenght - 1].ToString());
+            }
+            if (secondLenght > 1)
+            {
+                first = int.Parse(x.ToString()[second - 1].ToString());
+
+            }
+        }
+    }
+}
---
+++ Week05/01-SortingAndSearching/SelectionSort/ReverseComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    public class ReverseComparer<T> : IComparer<T>
+    {
+        private IComparer<T> comparer;
+
+        internal ReverseComparer(IComparer<T> compar)
+        {
+            this.comparer = compar;
+        }
+
+        public int Compare(T x, T y)
+        {
+            return -comparer.Compare(x, y);
+        }
+    }
+}
