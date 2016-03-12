40bd95fcac96b8c677f9a46972b71d1b53a71ffa
ItsUpToYou <v_kapitanov@yahoo.com>
2016-03-12 15:49:48 +0200

    C# week04

---
+++ Week04/02-More-Generics-And-Collections/MapApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapLib;
+
+namespace MapApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map< int, string> test = new Map<int,string>();
+            test.Add(1, "Dominc");
+            test.Add(2, "Select");
+            test.Add(3, "Create");
+            Console.WriteLine(test[1]);
+            Console.WriteLine(test[2]);
+            Console.WriteLine(test[3]);
+            test[5] = "Dian";
+            Console.WriteLine(test[5]);
+            Console.WriteLine();
+
+            test.RemoveKey(1);
+
+            Console.WriteLine();
+            Console.WriteLine(test[2]);
+            Console.WriteLine(test[3]);
+            Console.WriteLine(test[5]);
+            Console.WriteLine();
+
+            Console.WriteLine(test.ContainsKey(2));
+            Console.WriteLine(test.ContainsValue("Dian"));
+        }
+    }
+}
---
+++ Week04/02-More-Generics-And-Collections/MapLib/Map.cs
@@ -0,0 +1,92 @@
+﻿using DynamicArrayLib;
+using System;
+using System.Collections.Generic;
+
+namespace MapLib
+{
+    public class Map<T, U>
+    {
+        DynamicArray<T> keys;
+        DynamicArray<U> values;
+
+        public Map()
+        {
+            this.keys = new DynamicArray<T>();
+            this.values = new DynamicArray<U>();
+        }
+
+        public void Clear()
+        {
+            this.keys.Clear();
+            this.values.Clear();
+        }
+
+        public void Add(T key, U value)
+        {
+            if (key.Equals(0))
+            {
+                throw new ArgumentException("The key must not be Zero!");
+            }
+
+            if (ContainsKey(key))
+            {
+                throw new ArgumentException("The key already exist!");
+            }
+            this.keys.Add(key);
+            this.values.Add(value);
+        }
+
+        public bool ContainsKey(T key)
+        {
+            return this.keys.Contains(key);
+        }
+
+        public bool ContainsValue(U value)
+        {
+            return this.values.Contains(value);
+        }
+
+        public void RemoveKey(T key)
+        {
+
+            if (ContainsKey(key))
+            {
+                U value = this[key];
+                this.keys.Remove(key);
+                this.values.Remove(value);
+            }
+        }
+
+        public U this[T key]
+        {
+            get
+            {
+                if (this.keys.Contains(key))
+                {
+                    var index = this.keys.IndexOf(key);
+                    return this.values[index];
+                }
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                if (key.Equals(0))
+                {
+                    throw new Exception("The key musn't be zero!");
+                }
+
+                if (this.keys.Contains(key))
+                {
+                    var index = keys.IndexOf(key);
+                    this.values[index] = value;
+                }
+                else
+                {
+                    this.keys.Add(key);
+                    this.values.Add(value);
+                }
+            }
+        }
+
+    }
+}
