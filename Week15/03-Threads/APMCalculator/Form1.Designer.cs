d1c1f3ab571ebd2e0259309fee5940faff3ef408
ItsUpToYou <v_kapitanov@yahoo.com>
2016-03-13 14:56:05 +0200

    Threads

---
+++ Week15/03-Threads/APMCalculator/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace APMCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.labelTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(61, 76);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click Me!";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // labelTxt
+            // 
+            this.labelTxt.AutoSize = true;
+            this.labelTxt.Location = new System.Drawing.Point(188, 81);
+            this.labelTxt.Name = "labelTxt";
+            this.labelTxt.Size = new System.Drawing.Size(35, 13);
+            this.labelTxt.TabIndex = 1;
+            this.labelTxt.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelTxt);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label labelTxt;
+    }
+}
+
---
+++ Week15/03-Threads/APMCalculator/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APMCalculator
+{
+    public partial class Form1 : Form
+    {
+        double result;
+        Stopwatch stopwatch = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+
+            Thread thread = new Thread(APMCalculate);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+
+        volatile int count = 0;
+        
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            Object locker = new Object();
+
+            lock (locker)
+            {
+                count++;
+            }
+        }
+
+        public void APMCalculate()
+        {
+            Object obj = new Object();
+            while (true)
+            {
+                lock (obj)
+                {
+                    result = count / stopwatch.Elapsed.TotalMinutes;
+                }
+
+                Thread.Sleep(1500);
+
+                BeginInvoke((Action)delegate ()
+                {
+                    ResultCalc();
+                });
+            }
+        }
+
+        public void ResultCalc()
+        {
+            labelTxt.Text = result.ToString();
+        }
+    }
+}
