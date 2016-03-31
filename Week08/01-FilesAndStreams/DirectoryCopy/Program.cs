926130d0f4e0c6a6cfc6d7fa2cbc54af880ea478
ItsUpToYou <v_kapitanov@yahoo.com>
2016-04-01 01:31:03 +0300

    FilesAndStreams
    C# week-8

---
+++ Week08/01-FilesAndStreams/DirectoryCopy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace DirectoryCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string existDir =  @"../../TestDirectory";
+            string newtargetDir =  @"../../TestDirectoryNew";
+
+            CopyDirectory(existDir, newtargetDir, true);
+
+        }
+
+        static void CopyDirectory(string existDir, string targetDir, bool isRecursive)
+        {
+            DirectoryInfo directories = new DirectoryInfo(existDir);
+
+            if (!directories.Exists)
+            {
+                throw new DirectoryNotFoundException(string.Format("The given directory not exist : {0}", directories.FullName));
+            }
+
+            if (!Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+            }
+
+            FileInfo[] files = directories.GetFiles();
+            foreach (var file in files)
+            {
+                string temporaryPath = Path.Combine(targetDir, file.Name);
+                file.CopyTo(temporaryPath, true);
+            }
+
+            if (isRecursive)
+            {
+                DirectoryInfo[] dirs = directories.GetDirectories();
+                foreach (var dir in dirs)
+                {
+                    string tempPath = Path.Combine(targetDir, dir.Name);
+                    CopyDirectory(dir.FullName, tempPath, isRecursive);
+                }
+            }
+        }
+    }
+}
---
+++ Week08/01-FilesAndStreams/FileNamesHistogram/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileNamesHistogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathDir = @"../../TestDirectory";
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            GetFiles(pathDir, dict);
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+
+        /// <summary>
+        /// Recursive method for looking and counting files
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="dict"></param>
+        private static void GetFiles(string path, Dictionary<string,int> dict)
+        {
+            
+            DirectoryInfo[] dir = new DirectoryInfo(path).GetDirectories();
+            FileInfo[] fileInfo = new DirectoryInfo(path).GetFiles();
+
+            if (fileInfo != null)
+            {
+                foreach (var files in fileInfo)
+                {
+                    if (dict.ContainsKey(files.Name))
+                    {
+                        dict[files.Name]++;
+                    }
+                    else
+                    {
+                        dict.Add(files.Name, 1);
+                    }
+                }
+            }
+
+            if (dir != null)
+            {
+                foreach (var dirInfo in dir)
+                {
+                    GetFiles(dirInfo.FullName, dict);
+                }
+            }
+        }
+    }
+}
